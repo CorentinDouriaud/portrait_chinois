1ac650ef980c4fcdca4df940e955e4748ef4bccf
Corentin <corentin.douriaud@gmail.com>
2023-07-29 23:51:25 +0200

    AJouts de questions et de 4 catégories

--- src/App.js
+++ src/App.js
@@ -2,51 +2,90 @@ import './App.css';
 import AddSuspense from './AddSuspense'
 import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import { questionJaunes, questionRouges, questionVertes } from './Questions';
+import { questionJaunes, questionRouges, questionVertes, questionNoires, questionPourpres, questionOranges, questionBleues } from './Questions';
+
+const rouge = "#d42a2a"
+const bleu = "#22147d"
+const vert = "#3e992e"
+const jaune = "#d9d90f"
+const pourpre = "#630e2f"
+const noir = "#000000"
+const orange = "#e6830b"
 
 const styleBoutonRouge ={
   borderRadius: 35,
-  backgroundColor: "#2b33bd",
+  backgroundColor: "#ffffff",
   padding: "18px 36px",
   fontSize: "18px",
-  color: "#d42a2a"
+  color: "#d42a2a",
+  width: "300px"
 }
 
 const styleBoutonJaune ={
   borderRadius: 35,
-  backgroundColor: "#2b33bd",
+  backgroundColor: "#ffffff",
   padding: "18px 36px",
   fontSize: "18px",
-  color: "#d9b61c"
+  color: "#d9d90f",
+  width: "300px"
 }
 
 const styleBoutonVert ={
   borderRadius: 35,
-  backgroundColor: "#2b33bd",
+  backgroundColor: "#ffffff",
   padding: "18px 36px",
   fontSize: "18px",
-  color: "#3e992e"
+  color: "#3e992e",
+  width: "300px"
 }
+
+
+  const styleBoutonNoir ={
+    borderRadius: 35,
+    backgroundColor: "#ffffff",
+    padding: "18px 36px",
+    fontSize: "18px",
+    color: "#000000",
+    width: "300px"
+  }
+
+  const styleBoutonOrange ={
+    borderRadius: 35,
+    backgroundColor: "#ffffff",
+    padding: "18px 36px",
+    fontSize: "18px",
+    color: "#e6830b",
+    width: "300px"
+  }
+
+  const styleBoutonBleu ={
+    borderRadius: 35,
+    backgroundColor: "#ffffff",
+    padding: "18px 36px",
+    fontSize: "18px",
+    color: "#3522c7",
+    width: "300px"
+  }
+
+  const styleBoutonPourpre ={
+    borderRadius: 35,
+    backgroundColor: "#ffffff",
+    padding: "18px 36px",
+    fontSize: "18px",
+    color: "#630e2f",
+    width: "300px"
+  }
 
 function App() {
   
   const [couleur, setCouleur] = useState("grey")
-  const [personState, setPersonState] = useState("Quelle est ton empathie envers la nature ?")
-  const questionVertesAlea = () => {
-    const randomNumber = Math.floor(Math.random() * questionVertes.length);
-    setPersonState(questionVertes[randomNumber])
-    console.log(personState)
+  const [personState, setPersonState] = useState("Clique sur un bouton pour avoir ta première question !")
+
+const questionsChoisie = (tableauQuestions) => {
+  const randomNumber = Math.floor(Math.random() * tableauQuestions.length);
+  setPersonState(tableauQuestions[randomNumber])
 }
-const questionRougesAlea = () => {
-  const randomNumber = Math.floor(Math.random() * questionRouges.length);
-  setPersonState(questionRouges[randomNumber])
-  console.log(personState)
-}
-const questionJaunesAlea = () => {
-  const randomNumber = Math.floor(Math.random() * questionJaunes.length);
-  setPersonState(questionJaunes[randomNumber])
-  console.log(personState)
-}
+
   const click = couleur => {
     setCouleur(couleur)
   }
@@ -60,11 +99,19 @@ const questionJaunesAlea = () => {
         <p>
           <AddSuspense texte={personState} key={personState}/>
           <p></p>
-          <Button variant="contained" style={styleBoutonRouge} onClick={()=>{click("#d42a2a"); questionRougesAlea()}}>Questions Rouges</Button>
+          <Button variant="contained" style={styleBoutonJaune} onClick={()=>{click(jaune); questionsChoisie(questionJaunes)}}>Questions Jaunes</Button>
           <p></p>
-          <Button variant="contained" style={styleBoutonJaune} onClick={()=>{click("#d9b61c"); questionJaunesAlea()}}>Questions Jaunes</Button>
+          <Button variant="contained" style={styleBoutonOrange} onClick={()=>{click(orange); questionsChoisie(questionOranges)}}>Questions Oranges</Button>
           <p></p>
-          <Button variant="contained" style={styleBoutonVert} onClick={()=>{click("#3e992e"); questionVertesAlea()}}>Questions Vertes</Button>
+          <Button variant="contained" style={styleBoutonVert} onClick={()=>{click(vert); questionsChoisie(questionVertes)}}>Questions Vertes</Button>
+          <p></p>
+          <Button variant="contained" style={styleBoutonBleu} onClick={()=>{click(bleu); questionsChoisie(questionBleues)}}>Questions Bleues</Button>
+          <p></p>
+          <Button variant="contained" style={styleBoutonNoir} onClick={()=>{click(noir); questionsChoisie(questionNoires)}}>Questions Noires</Button>
+          <p></p>
+          <Button variant="contained" style={styleBoutonRouge} onClick={()=>{click(rouge); questionsChoisie(questionRouges)}}>Questions Rouges</Button>
+          <p></p>
+          <Button variant="contained" style={styleBoutonPourpre} onClick={()=>{click(pourpre); questionsChoisie(questionPourpres)}}>Questions Pourpres</Button>
         </p>
       </header>
             
